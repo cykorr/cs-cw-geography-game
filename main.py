ed0650d59d13fadb4e11605ba26fa6dd6a16ea3e
korr <pyk0rr@gmail.com>
2024-10-09 20:43:53 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import time
-score = 1
-win = 1
-total_score = 0
+total_score = 1
+cntrl_panel = "********69"
 
 print('''
 ----------------------------
@@ -12,8 +11,7 @@ welcome to geography quiz!!!
 ''')
 user = str(input('''what do u want ur username to be?
 '''))
-# creates usernameVAR to hide true user and hidden ids
-username = "@" + user
+username = "@" + user # creates usernameVAR to hide true user and hidden ids
 # admin debug panel, ignore
 if user.lower() == "admin123":
     debug_mode = input('''administrator detected.
@@ -23,7 +21,7 @@ engage debug mode? (y/n)''')
         print("DEBUG MODE ENABLED.")
         username = "@admin"
         #hiding free points generator...
-        user = "super_secret_admin_password"
+        user = cntrl_panel
 # patching admin loophole
 elif user.lower() == "super_secret_admin_password":
     print("loophole detected...")
@@ -41,51 +39,78 @@ time.sleep(2.0) # waiting makes it more dramatic
 # q1
 q1 = str(input('''how old is the earth rounded to 1 number in the billions?
 answer : '''))
-if q1 == "5" or user == "super_secret_admin_password":
+if q1 == "5" or user == cntrl_panel:
     print("answer is correct", username,"! your current score is : 1")
-    total_score = score + win
-    score = total_score
+    total_score = 1
 else:
     print("womp womp, that's incorrect")
-    core = total_score
 
 time.sleep(2.0)
 
 # q2
 print("----------------------------") # this is to make a new line
 q2 = str(input('''how many continents are there? : '''))
-if q2 == "7" or user == "super_secret_admin_password":
+if q2 == "7" or user == cntrl_panel:
     print("answer is correct! your current score is :", str(total_score))
-    total_score = score + win
-    score = total_score
+    total_score = total_score + 1
 else:
-    print("womp womp, that's incorrect")
-    score = total_score    
+    print("womp womp, that's incorrect") 
 
 time.sleep(2.0)
 
 # q3
 print("----------------------------") # this is to make a new line
 q3 = str(input("what's the hardest material on earth? : "))
-if q3.lower() == "diamond" or user == "super_secret_admin_password":
+if q3.lower() == "diamond" or user == cntrl_panel:
     print("answer is correct! your current score is :", str(total_score))
-    total_score = score + win
-    score = total_score
+    total_score = total_score + 1
 else:
-    print("womp womp, that's incorrect")
-    score = total_score    
+    print("womp womp, that's incorrect")  
 
 time.sleep(2.0)
 
 # q4
 print("----------------------------") # this is to make a new line
-q3 = str(input("which continent is russia located in? : "))
-if q3.lower() == "asia" or q3.lower() == "europe" or user == "super_secret_admin_password":
+q4 = str(input("which continent is russia located in? : "))
+if q4.lower() == "asia" or q4.lower() == "europe" or user == cntrl_panel:
     print("answer is correct! your current score is :", str(total_score))
-    total_score = score + win
-    score = total_score
+    total_score = total_score + 1
 else:
-    print("womp womp, that's incorrect")
-    score = total_score    
+    print("womp womp, that's incorrect") 
 
 time.sleep(2.0)
+
+# q5
+print("----------------------------") # this is to make a new line
+q5 = str(input('''What is the name of the tallest mountain in the world?
+a) mount everest
+b) mount K2
+c) mount tai 
+answer : '''))
+if q5.lower() == "a" or user == cntrl_panel:
+    print("answer is correct! your current score is :", str(total_score))
+    total_score = total_score + 1
+else:
+    print("womp womp, that's incorrect")   
+
+time.sleep(2.0)
+
+# q6
+print("----------------------------") # this is to make a new line
+q6 = str(input('''What's the name of the longest river in Africa?
+a) amazon river
+b) nile river
+c) sink
+answer : '''))
+if q6.lower() == "b" or user == cntrl_panel:
+    print("answer is correct! your current score is :", str(total_score))
+    total_score = total_score + 1
+elif q6.lower() == "c":
+    print("nah bruda r u dumb :skull: deducting a point for that")
+    total_score = total_score - 1
+    print("your current score is :", str(total_score))
+else:
+    print("womp womp, that's incorrect")   
+
+time.sleep(2.0)
+
